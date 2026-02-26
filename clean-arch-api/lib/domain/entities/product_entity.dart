2fb3291fd7348f65202e2817db374bc382c5b002
katharinefernandes <katharinefv@gmail.com>
2026-02-25 22:39:06 -0300

    add - clean arch + communication with api

---
+++ clean-arch-api/lib/domain/entities/product_entity.dart
@@ -0,0 +1,12 @@
+class ProductEntity {
+  final int id;
+  final String title;
+  final String price;
+
+  ProductEntity({
+    required this.id, 
+    required this.title, 
+    required this.price 
+  });  
+
+}
